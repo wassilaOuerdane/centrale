06986d535d0bfb93f724f05d81946aefe34fd4c3
AleBlondin <alexandreblondin@laposte.net>
2020-05-04 21:23:30 +0200

    :sparkles: use knobs in storybook

--- client/src/stories/1-Button.stories.js
+++ client/src/stories/1-Button.stories.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
+import React from "react";
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
 
 export default {
-  title: 'Button',
-  component: Button,
+  title: "Storybook Knobs",
+  decorators: [withKnobs]
 };
+// Add the `withKnobs` decorator to add knobs support to your stories.
+// You can also configure `withKnobs` as a global decorator.
 
-export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
-
-export const Emoji = () => (
-  <Button onClick={action('clicked')}>
-    <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
-    </span>
-  </Button>
+// Knobs for React props
+export const withAButton = () => (
+  <button disabled={boolean("Disabled", false)}>
+    {text("Label", "Hello Storybook")}
+  </button>
 );
+
+// Knobs as dynamic variables.
+export const asDynamicVariables = () => {
+  const name = text("Name", "James");
+  const age = number("Age", 35);
+  const content = `I am ${name} and I'm ${age} years old.`;
+
+  return <div>{content}</div>;
+};
