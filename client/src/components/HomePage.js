4b0c774ca9d21df79ded3381173d4faeae3f4917
AleBlondin <alexandreblondin@laposte.net>
2020-06-02 18:50:01 +0200

    :sparkles: add routing to app

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="HomePage">
+      <header className="HomePage-header">
+        <img src={logo} className="HomePage-logo" alt="logo" />
+        <p>
+          Edit <code>src/HomePage.js</code> and save to reload.
+        </p>
+        <a
+          className="HomePage-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default HomePage;
